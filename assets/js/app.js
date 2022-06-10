948e0510b4e40208366c7b2d775fe6a9d2dc9aa1
diegolu7 <diegolu7@gmail.com>
2022-06-10 17:15:15 -0300

    add spiner

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,23 +6,38 @@ import { mostrarMensaje } from "./MostrarMensaje.js";
 
 const input = document.querySelector("#id");
 const btn = document.querySelector("#btn");
-
+const spiner = document.querySelector("#loading");
 
 btn.addEventListener('click', (e) => {
-  e.preventDefault();
-
   LimpiarCard();
+  spiner.style.display="inline-block";
+  setTimeout( () => {
+    try {
+      e.preventDefault();
+      
+      LimpiarCard();
 
-  guardar_localStorage(data,"pizzas");
-  const dataLS = obtener_localStorage("pizzas");
+      guardar_localStorage(data,"pizzas");
+      const dataLS = obtener_localStorage("pizzas");
 
-  if(input.value < -1 || input.value > 7){
-    mostrarMensaje("Pizza no encontrada.");
+      if(input.value < -1 || input.value > 7){
+        mostrarMensaje("Pizza no encontrada.");
+      }
+
+      for (let i = 0; i < dataLS.length; i++) {
+        if (dataLS[i].id == input.value) {
+          CargarCard(dataLS[i].nombre, dataLS[i].precio, dataLS[i].img, dataLS[i].ingredientes,)
+        }
+      }
   }
-
-  for (let i = 0; i < dataLS.length; i++) {
-    if (dataLS[i].id == input.value) {
-      CargarCard(dataLS[i].nombre, dataLS[i].precio, dataLS[i].img, dataLS[i].ingredientes,)
-    }
+  finally {
+      spiner.style.display="none";
   }
+ }, 500);
 });
+
+
+
+
+
+
