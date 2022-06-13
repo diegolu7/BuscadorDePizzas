f2892a0d327dc3ae30648a2dfc3754639e5eb813
diegolu7 <diegolu7@gmail.com>
2022-06-13 01:38:33 -0300

    fix input empty

--- assets/js/CargarCard.js
+++ assets/js/CargarCard.js
@@ -1,12 +1,12 @@
 export function CargarCard(nombre,precio,img,ingredientes){
   const card = document.querySelector("#respuesta");
   card.classList.add("card");
+  card.classList.add("card-style");
   card.innerHTML=
         `<h2 class="card__title">Pizza: ${nombre} $${precio}</h2>
         <img class="card__img" src="${img}" alt="${nombre}-pizza">
         <h3 class="card__subtitle">Ingredientes</h3>
         <ul class="card__list">
-
         </ul>`;
 
  const ul = document.querySelector("#respuesta > ul");
--- assets/js/LimpiarCard.js
+++ assets/js/LimpiarCard.js
@@ -2,4 +2,6 @@ export function LimpiarCard(){
   const card = document.querySelector("#respuesta");
   card.innerHTML="";
   card.classList.remove("card");
+  card.classList.remove("card-style");
+
 }
